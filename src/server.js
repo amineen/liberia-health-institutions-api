76bb10c12b2599c6b95e351900f26381b6ad5963
Aaron Mineen <amineen64@hotmail.com>
2020-11-19 05:48:41 +0000

    Testing for deployment

--- src/server.js
+++ src/server.js
@@ -15,8 +15,7 @@ app.use(express.json());
 //Enable cors
 app.use(cors());
 //Routes
-//app.use('/api/v1/health-institutions', router );
-app.use('/', router );
+app.use('/api/v1/health-institutions', router );
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
