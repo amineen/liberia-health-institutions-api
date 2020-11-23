73b6e995173edbfd688dade93e4e10c4cc9bb306
Aaron Mineen <amineen64@hotmail.com>
2020-11-23 22:43:34 +0000

    Remove github work flow

--- src/server.js
+++ src/server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(cors());
 //Routes
 app.use('/api/v1/health-institutions', router );
+//exports.libHealthInstitutionsAPI = app;
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
